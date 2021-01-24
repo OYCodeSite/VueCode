f0b219bbfbcce697113a5e647da4a43dcb4213b0
OYCodeSite <oy2097291754@gmail.com>
2021-01-25 01:13:55 +0800

    first commit

---
+++ VueTest/13_插件/vue-myPlugin.js
@@ -0,0 +1,23 @@
+/*
+    自定义插件
+*/
+(function(){
+    const MyPlugin = {};
+    MyPlugin.install = function(Vue, options){
+        // 1.添加全局方法或属性
+        Vue.myGlobalMethod = function(){
+            alert('Vue 函数对象方法执行');
+        };
+
+        // 2.添加全局资源
+        Vue.directive('my-directive', function(el,binding){
+            el.innerHTML = "MyPlugin my-directive " + binding.value;
+        });
+
+        // 3.添加实例方法
+        Vue.prototype.$myMethod = function(){
+            alert('vue 实例对象方法执行');
+        };
+    };
+    window.MyPlugin = MyPlugin;
+})()
